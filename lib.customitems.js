1232795edd94ceb8ddf476b13c4d6660d658259b
Herobrine331 <72279272+Herobrine331@users.noreply.github.com>
2025-09-18 19:15:33 -0400

    l
    trest

--- lib.customitems.js
+++ lib.customitems.js
@@ -1,13 +1,13 @@
 // Library to make adding custom items with EaglerForgeInjector much easier.
 (function LibItems() {
-    ModAPI.meta.title("LibCustomItems");
-    ModAPI.meta.credits("By ZXMushroom63");
-    ModAPI.meta.icon("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAQdJREFUOE9jZGBg+M9AAWAEGbBl2QmyjPCJsmAgaABbdybc8F+l01EswmsATONXLi4GYSkpBgZ+foY1O3cyuHWuhhuC1QBkjf///QMrFtHWZmD4+BHDEBQDUGzU1ITb8ubqVZyGoBjwsCONQYqXl0FYU5MBpAlsKxRgM+STUwoDhgG66upgZ4IAuiEooRcXx/DpCRuqAU97shg0jYzgfsVpSFwcg5mZGcOedRewGDBhAgPDokUohsBthmoE8U+dOoXdBfHHjoElUQxB03i9oABspnTJNFQXgARB3oAbwsAAdirMRmSNMFdhTQcwQ/BpxGsAzCUwRSCn4gJE5QV8uQxuAFlZEaoJAKrYrAHl38o6AAAAAElFTkSuQmCC");
-    ModAPI.meta.description("Library to make adding basic custom items easier.");
-    ModAPI.events.newEvent("lib:libcustomitems:loaded");
+    PluginAPI.meta.title("LibCustomItems");
+    PluginAPI.meta.credits("By ZXMushroom63");
+    PluginAPI.meta.icon("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAQdJREFUOE9jZGBg+M9AAWAEGbBl2QmyjPCJsmAgaABbdybc8F+l01EswmsATONXLi4GYSkpBgZ+foY1O3cyuHWuhhuC1QBkjf///QMrFtHWZmD4+BHDEBQDUGzU1ITb8ubqVZyGoBjwsCONQYqXl0FYU5MBpAlsKxRgM+STUwoDhgG66upgZ4IAuiEooRcXx/DpCRuqAU97shg0jYzgfsVpSFwcg5mZGcOedRewGDBhAgPDokUohsBthmoE8U+dOoXdBfHHjoElUQxB03i9oABspnTJNFQXgARB3oAbwsAAdirMRmSNMFdhTQcwQ/BpxGsAzCUwRSCn4gJE5QV8uQxuAFlZEaoJAKrYrAHl38o6AAAAAElFTkSuQmCC");
+    PluginAPI.meta.description("Library to make adding basic custom items easier.");
+    PluginAPI.events.newEvent("lib:libcustomitems:loaded");
     function libServerside() {
-        var packetblockchange = ModAPI.reflect.getClassByName("S23PacketBlockChange").constructors.find(x => { return x.length === 2 });
-        var sendPacket = ModAPI.reflect.getClassByName("NetHandlerPlayServer").methods.sendPacket.method;
+        var packetblockchange = PluginAPI.reflect.getClassByName("S23PacketBlockChange").constructors.find(x => { return x.length === 2 });
+        var sendPacket = PluginAPI.reflect.getClassByName("NetHandlerPlayServer").methods.sendPacket.method;
         globalThis.LCI_REGISTRY ||= [];
         globalThis.LCI_RMBEVENTS ||= {};
         globalThis.LCI_LMBEVENTS ||= {};
@@ -18,17 +18,17 @@
                 return globalThis.LCI_ITEMDB[tag] || null;
             }
         };
-        var useName = ModAPI.util.getMethodFromPackage("net.minecraft.network.NetHandlerPlayServer", "processPlayerBlockPlacement");
-        var oldUse = ModAPI.hooks.methods[useName];
-        ModAPI.hooks.methods[useName] = function ($this, packet) {
+        var useName = PluginAPI.util.getMethodFromPackage("net.minecraft.network.NetHandlerPlayServer", "processPlayerBlockPlacement");
+        var oldUse = PluginAPI.hooks.methods[useName];
+        PluginAPI.hooks.methods[useName] = function ($this, packet) {
             if ($this?.$playerEntity?.$inventory && $this.$playerEntity.$inventory.$getCurrentItem()) {
                 var item = $this.$playerEntity.$inventory.$getCurrentItem();
-                if (item.$stackTagCompound && item.$stackTagCompound.$hasKey(ModAPI.util.str("display"), 10)) {
-                    var displayTag = item.$stackTagCompound.$getCompoundTag(ModAPI.util.str("display"));
-                    if (displayTag.$hasKey(ModAPI.util.str("Lore"), 9)) {
-                        var loreTag = displayTag.$getTag(ModAPI.util.str("Lore"));
-                        if (loreTag.$tagCount() > 0 && globalThis.LCI_REGISTRY.includes(ModAPI.util.ustr(loreTag.$getStringTagAt(0)))) {
-                            var cid = ModAPI.util.ustr(loreTag.$getStringTagAt(0));
+                if (item.$stackTagCompound && item.$stackTagCompound.$hasKey(PluginAPI.util.str("display"), 10)) {
+                    var displayTag = item.$stackTagCompound.$getCompoundTag(PluginAPI.util.str("display"));
+                    if (displayTag.$hasKey(PluginAPI.util.str("Lore"), 9)) {
+                        var loreTag = displayTag.$getTag(PluginAPI.util.str("Lore"));
+                        if (loreTag.$tagCount() > 0 && globalThis.LCI_REGISTRY.includes(PluginAPI.util.ustr(loreTag.$getStringTagAt(0)))) {
+                            var cid = PluginAPI.util.ustr(loreTag.$getStringTagAt(0));
                             if (!globalThis.LCI_RMBEVENTS[cid]) {
                                 return oldUse.apply(this, [$this, packet]);
                             }
@@ -37,10 +37,10 @@
                                 return 0;
                             }
                             var r = globalThis.LCI_RMBEVENTS[cid].call(globalThis,
-                                new Proxy($this.$playerEntity, ModAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf),
-                                new Proxy($this.$serverController.$worldServerForDimension($this.$playerEntity.$dimension), ModAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf),
-                                new Proxy(item, ModAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf),
-                                new Proxy(packet[positionTag], ModAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf));
+                                new Proxy($this.$playerEntity, PluginAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf),
+                                new Proxy($this.$serverController.$worldServerForDimension($this.$playerEntity.$dimension), PluginAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf),
+                                new Proxy(item, PluginAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf),
+                                new Proxy(packet[positionTag], PluginAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf));
                             if (!r) {
                                 return oldUse.apply(this, [$this, packet]);
                             }
@@ -51,24 +51,24 @@
             }
             return oldUse.apply(this, [$this, packet]);
         }
-        var digName = ModAPI.util.getMethodFromPackage("net.minecraft.network.NetHandlerPlayServer", "processPlayerDigging");
-        var oldDig = ModAPI.hooks.methods[digName];
-        ModAPI.hooks.methods[digName] = function ($this, packet) {
+        var digName = PluginAPI.util.getMethodFromPackage("net.minecraft.network.NetHandlerPlayServer", "processPlayerDigging");
+        var oldDig = PluginAPI.hooks.methods[digName];
+        PluginAPI.hooks.methods[digName] = function ($this, packet) {
             if ($this?.$playerEntity?.$inventory && $this.$playerEntity.$inventory.$getCurrentItem()) {
                 var item = $this.$playerEntity.$inventory.$getCurrentItem();
-                if (item.$stackTagCompound && item.$stackTagCompound.$hasKey(ModAPI.util.str("display"), 10)) {
-                    var displayTag = item.$stackTagCompound.$getCompoundTag(ModAPI.util.str("display"));
-                    if (displayTag.$hasKey(ModAPI.util.str("Lore"), 9)) {
-                        var loreTag = displayTag.$getTag(ModAPI.util.str("Lore"));
-                        if (loreTag.$tagCount() > 0 && globalThis.LCI_REGISTRY.includes(ModAPI.util.ustr(loreTag.$getStringTagAt(0)))) {
-                            var cid = ModAPI.util.ustr(loreTag.$getStringTagAt(0));
+                if (item.$stackTagCompound && item.$stackTagCompound.$hasKey(PluginAPI.util.str("display"), 10)) {
+                    var displayTag = item.$stackTagCompound.$getCompoundTag(PluginAPI.util.str("display"));
+                    if (displayTag.$hasKey(PluginAPI.util.str("Lore"), 9)) {
+                        var loreTag = displayTag.$getTag(PluginAPI.util.str("Lore"));
+                        if (loreTag.$tagCount() > 0 && globalThis.LCI_REGISTRY.includes(PluginAPI.util.ustr(loreTag.$getStringTagAt(0)))) {
+                            var cid = PluginAPI.util.ustr(loreTag.$getStringTagAt(0));
                             if (!globalThis.LCI_LMBEVENTS[cid]) {
                                 return oldDig.apply(this, [$this, packet]);
                             }
-                            var $status = ModAPI.util.getNearestProperty(packet, "$status");
+                            var $status = PluginAPI.util.getNearestProperty(packet, "$status");
                             var statusTag = Object.keys(packet[$status]).find(x => { return x.startsWith("$name") });
                             var positionTag = Object.keys(packet).filter(x => { return x.startsWith("$position") })[0];
-                            var stat = ModAPI.util.unstr(packet[$status][statusTag]);
+                            var stat = PluginAPI.util.unstr(packet[$status][statusTag]);
                             if (stat === "START_DESTROY_BLOCK") {
                                 sendPacket($this, packetblockchange($this.$serverController.$worldServerForDimension($this.$playerEntity.$dimension), packet[positionTag]));
                             }
@@ -80,10 +80,10 @@
                             }
 
                             var r = globalThis.LCI_LMBEVENTS[cid].call(globalThis,
-                                new Proxy($this.$playerEntity, ModAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf),
-                                new Proxy($this.$serverController.$worldServerForDimension($this.$playerEntity.$dimension), ModAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf),
-                                new Proxy(item, ModAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf),
-                                new Proxy(packet[positionTag], ModAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf));
+                                new Proxy($this.$playerEntity, PluginAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf),
+                                new Proxy($this.$serverController.$worldServerForDimension($this.$playerEntity.$dimension), PluginAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf),
+                                new Proxy(item, PluginAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf),
+                                new Proxy(packet[positionTag], PluginAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf));
                             if (!r) {
                                 return oldDig.apply(this, [$this, packet]);
                             }
@@ -111,16 +111,16 @@
         if (data.craftingExtra) {
             globalThis.LCI_RECIPEEVENTS[data.tag] = new Function("itemstack", data.craftingExtra);
         }
-        var ObjectClass = ModAPI.reflect.getClassById("java.lang.Object").class;
+        var ObjectClass = PluginAPI.reflect.getClassById("java.lang.Object").class;
         function ToChar(char) {
-            return ModAPI.reflect.getClassById("java.lang.Character").staticMethods.valueOf.method(char[0].charCodeAt(0));
+            return PluginAPI.reflect.getClassById("java.lang.Character").staticMethods.valueOf.method(char[0].charCodeAt(0));
         }
-        ModAPI.dedicatedServer.appendCode(`(function () {
+        PluginAPI.dedicatedServer.appendCode(`(function () {
             function handler() {
                 LCI_registerItem(${JSON.stringify(data)});
-                ModAPI.removeEventListener("tick", handler);
+                PluginAPI.removeEventListener("tick", handler);
             }
-            ModAPI.addEventListener("tick", handler);
+            PluginAPI.addEventListener("tick", handler);
         })()`);
         var recipeInternal = [];
         Object.keys(data.recipeLegend).forEach((key) => {
@@ -128,35 +128,35 @@
             var ingredient = null;
             var schema = data.recipeLegend[key];
             if (schema.type === "block") {
-                ingredient = ModAPI.blocks[schema.id].getRef();
+                ingredient = PluginAPI.blocks[schema.id].getRef();
             } else {
-                ingredient = ModAPI.items[schema.id].getRef();
+                ingredient = PluginAPI.items[schema.id].getRef();
             }
             recipeInternal.push(ingredient);
         });
-        var recipeContents = data.recipe.flatMap(x => { return ModAPI.util.str(x) });
-        var recipe = ModAPI.util.makeArray(ObjectClass, recipeContents.concat(recipeInternal));
+        var recipeContents = data.recipe.flatMap(x => { return PluginAPI.util.str(x) });
+        var recipe = PluginAPI.util.makeArray(ObjectClass, recipeContents.concat(recipeInternal));
 
-        var testItem = ModAPI.reflect.getClassById("net.minecraft.item.ItemStack").constructors[4](ModAPI.items[data.base].getRef(), data.qty);
-        testItem.$stackTagCompound = ModAPI.reflect.getClassById("net.minecraft.nbt.NBTTagCompound").constructors[0]();
-        testItem.$stackTagCompound.$setTag(ModAPI.util.str("display"), ModAPI.reflect.getClassById("net.minecraft.nbt.NBTTagCompound").constructors[0]());
-        var displayTag = testItem.$stackTagCompound.$getCompoundTag(ModAPI.util.str("display"));
-        displayTag.$setString(ModAPI.util.str("Name"), ModAPI.util.str(data.name));
-        var lore = ModAPI.reflect.getClassById("net.minecraft.nbt.NBTTagList").constructors[0]();
-        lore.$appendTag(ModAPI.reflect.getClassById("net.minecraft.nbt.NBTTagString").constructors.filter(x => { return x.length === 1 })[0](ModAPI.util.str(data.tag)));
-        displayTag.$setTag(ModAPI.util.str("Lore"), lore);
+        var testItem = PluginAPI.reflect.getClassById("net.minecraft.item.ItemStack").constructors[4](PluginAPI.items[data.base].getRef(), data.qty);
+        testItem.$stackTagCompound = PluginAPI.reflect.getClassById("net.minecraft.nbt.NBTTagCompound").constructors[0]();
+        testItem.$stackTagCompound.$setTag(PluginAPI.util.str("display"), PluginAPI.reflect.getClassById("net.minecraft.nbt.NBTTagCompound").constructors[0]());
+        var displayTag = testItem.$stackTagCompound.$getCompoundTag(PluginAPI.util.str("display"));
+        displayTag.$setString(PluginAPI.util.str("Name"), PluginAPI.util.str(data.name));
+        var lore = PluginAPI.reflect.getClassById("net.minecraft.nbt.NBTTagList").constructors[0]();
+        lore.$appendTag(PluginAPI.reflect.getClassById("net.minecraft.nbt.NBTTagString").constructors.filter(x => { return x.length === 1 })[0](PluginAPI.util.str(data.tag)));
+        displayTag.$setTag(PluginAPI.util.str("Lore"), lore);
         if (globalThis.LCI_RECIPEEVENTS[data.tag]) {
-            globalThis.LCI_RECIPEEVENTS[data.tag](new Proxy(testItem, ModAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf));
+            globalThis.LCI_RECIPEEVENTS[data.tag](new Proxy(testItem, PluginAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf));
         }
-        globalThis.LCI_ITEMDB[data.tag] = new Proxy(testItem, ModAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf);
+        globalThis.LCI_ITEMDB[data.tag] = new Proxy(testItem, PluginAPI.util.TeaVM_to_Recursive_BaseData_ProxyConf);
 
-        var craftingManager = ModAPI.reflect.getClassById("net.minecraft.item.crafting.CraftingManager").staticMethods.getInstance.method();
+        var craftingManager = PluginAPI.reflect.getClassById("net.minecraft.item.crafting.CraftingManager").staticMethods.getInstance.method();
         if((data.useRecipe !== false) || (data.useRecipe !== "false")) {
-            ModAPI.hooks.methods.nmic_CraftingManager_addRecipe(craftingManager, testItem, recipe);
+            PluginAPI.hooks.methods.nmic_CraftingManager_addRecipe(craftingManager, testItem, recipe);
         }
     }
-    ModAPI.dedicatedServer.appendCode(libServerside);
-    ModAPI.dedicatedServer.appendCode("globalThis.LCI_registerItem = " + LCI_registerItem.toString());
+    PluginAPI.dedicatedServer.appendCode(libServerside);
+    PluginAPI.dedicatedServer.appendCode("globalThis.LCI_registerItem = " + LCI_registerItem.toString());
     window.LibCustomItems = {};
     LibCustomItems.registerItem = function register(data) {
         LCI_registerItem(data);
@@ -164,5 +164,5 @@
     LibCustomItems.makeItemStack = function makeItemStack(tag) {
         return globalThis.LCI_ITEMDB[tag] || null;
     }
-    ModAPI.events.callEvent("lib:libcustomitems:loaded", {});
+    PluginAPI.events.callEvent("lib:libcustomitems:loaded", {});
 })();
